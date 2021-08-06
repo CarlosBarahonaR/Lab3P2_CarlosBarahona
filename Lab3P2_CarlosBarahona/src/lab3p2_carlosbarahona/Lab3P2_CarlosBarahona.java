3373552afda3a7803c1ff4d0669a68cdc2f1c88f
CJBarahona <cbarahona@t-developing.com>
2021-08-06 15:11:54 -0600

    Creacion de login

--- Lab3P2_CarlosBarahona/src/lab3p2_carlosbarahona/Lab3P2_CarlosBarahona.java
+++ Lab3P2_CarlosBarahona/src/lab3p2_carlosbarahona/Lab3P2_CarlosBarahona.java
@@ -5,6 +5,9 @@
  */
 package lab3p2_carlosbarahona;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
@@ -16,6 +19,28 @@ public class Lab3P2_CarlosBarahona {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner r = new Scanner(System.in);
+        Programadores david = new Programadores("david", "1234", "David", "Apellido", 1, "Admin", 5, 50000, "Java", "9-5");
+        ArrayList<Administradores> admins = new ArrayList();
+        ArrayList<Clientes> clientes = new ArrayList();
+        ArrayList<Motoristas> motoristas = new ArrayList();
+        ArrayList<Programadores> programadores = new ArrayList();
+        ArrayList<Restaurante> restaurante = new ArrayList();
+        int opcion=0;
+        while(opcion!=3){
+            System.out.println("Bienvenido a Hugo\n"
+                    + "¿Qué desea hacer?\n"
+                    + "1) Iniciar sesión"
+                    + "2) Registrarse"
+                    + "3) Salir");
+            opcion=r.nextInt();
+            switch(opcion){
+            
+            
+            }
+        
+        }
+
     }
-    
+
 }
